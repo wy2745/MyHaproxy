62d2f501794ba087ca404c6eff7a1aaf2eb7321a
zjx <panda@pandadeMacBook-Pro.local>
2016-05-07 12:16:03 +0800

    init of the project

---
+++ src/main/java/graduationProject/MyHaproxyApplication.java
@@ -0,0 +1,12 @@
+package graduationProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHaproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHaproxyApplication.class, args);
+	}
+}
